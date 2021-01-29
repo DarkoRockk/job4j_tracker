a87b5bde810f85e3514cadabbb2574274da3b5c1
DarkoRockk <shadrin.dev@gmail.com>
2021-01-29 17:04:22 +0300

    3. Реализация профессий в коде [#455695]

---
+++ src/main/java/ru/job4j/oop/Pacient.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Pacient {
+    String name;
+    String symptomes;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymptomes() {
+        return symptomes;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Profession.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+import java.util.Date;
+
+public class Profession {
+    String name;
+    String surname;
+    String education;
+    Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+}

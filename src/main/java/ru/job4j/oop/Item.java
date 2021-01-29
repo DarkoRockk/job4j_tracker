ff514afd1cc59da8f2be488bfc3ec4061f17ec52
DarkoRockk <shadrin.dev@gmail.com>
2021-01-29 16:47:49 +0300

    2. Перегрузка конструктора [#455689]

---
+++ src/main/java/ru/job4j/oop/Item.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class Item {
+    private String name;
+    private int id;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}

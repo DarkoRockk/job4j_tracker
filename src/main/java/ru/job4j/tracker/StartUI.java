ab3a8616cc131aba802cad41987bab1c843e968f
DarkoRockk <shadrin.dev@gmail.com>
2021-02-02 20:16:31 +0300

    9. toString [#455705]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,10 @@ public class StartUI {
         Tracker track = new Tracker();
         track.add(new Item());
         System.out.println(track.findById(1));
+
+        Item item = new Item();
+        item.setName("Траляля");
+        item.setId(400);
+        System.out.println(item);
     }
 }

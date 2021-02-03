0f05b556a5a65315d2761f738c5444c8ac6d35ac
DarkoRockk <shadrin.dev@gmail.com>
2021-02-03 16:58:02 +0300

    3. Интерфейс Input [#455628]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passengers(int people) {
+
+    }
+
+    @Override
+    public int addFuel(int volume) {
+        return 0;
+    }
+}

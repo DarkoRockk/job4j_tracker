7683b40b0e5992d6425566e5aa884f662888dbca
DarkoRockk <shadrin.dev@gmail.com>
2021-02-11 19:24:23 +0300

    3. Банковские переводы [#455615]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -13,11 +13,13 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        List<Account> list = users.get(findByPassport(passport));
-        if (!list.contains(account)) {
-            list.add(account);
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> list = users.get(user);
+            if (!list.contains(account)) {
+                list.add(account);
+            }
         }
-        users.put(findByPassport(passport), list);
     }
 
     public User findByPassport(String passport) {
@@ -25,44 +27,38 @@ public class BankService {
         for (User key : users.keySet()) {
             if (key.getPassport().equals(passport)) {
                 user = key;
+                break;
             }
         }
         return user;
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        List<Account> list = users.get(findByPassport(passport));
-        if (list != null) {
-            for (Account acc : list) {
-                if (acc.getRequisite().equals(requisite)) {
-                    return acc;
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> list = users.get(user);
+            if (list != null) {
+                for (Account acc : list) {
+                    if (acc.getRequisite().equals(requisite)) {
+                        return acc;
+                    }
                 }
             }
         }
+
         return null;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        if (users.containsKey(findByPassport(srcPassport)) &&
-                users.containsKey(findByPassport(destPassport))) {
-            List<Account> srcList = users.get(findByPassport(srcPassport));
-            List<Account> destList = users.get(findByPassport(destPassport));
             Account src = findByRequisite(srcPassport, srcRequisite);
             Account dest = findByRequisite(destPassport, destRequisite);
-            int srcIndex = srcList.indexOf(src);
-            int destIndex = destList.indexOf(dest);
-            if (src.getBalance() >= amount) {
+            if (src != null && dest != null && src.getBalance() >= amount) {
                 src.setBalance(src.getBalance() - amount);
                 dest.setBalance(dest.getBalance() + amount);
-                srcList.set(srcIndex, src);
-                destList.set(destIndex, dest);
-                users.put(findByPassport(srcPassport), srcList);
-                users.put(findByPassport(destPassport), destList);
                 rsl = true;
             }
-        }
         return rsl;
     }
 }

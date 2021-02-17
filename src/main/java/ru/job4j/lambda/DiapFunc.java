7e815b3c1c8f9cf47ffa5632d6c79f2a92676491
DarkoRockk <shadrin.dev@gmail.com>
2021-02-17 22:50:09 +0300

    5. Подсчет функции в диапазоне. [#455423]

---
+++ src/main/java/ru/job4j/lambda/DiapFunc.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class DiapFunc {
+
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> result = new ArrayList<>();
+        for (; start < end; start++) {
+            result.add(func);
+        }
+        return result;
+    };
+}

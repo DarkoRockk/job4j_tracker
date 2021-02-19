1d81b3fa246ce7162ec42e5f214a5e51aba579d9
DarkoRockk <shadrin.dev@gmail.com>
2021-02-19 19:29:52 +0300

    3. Уникальность элементов и сортировка. [#455440]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -3,23 +3,14 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ListToMap {
-    public static void main(String[] args) {
-
-        List<Student> list = new ArrayList<>();
-        list.add(new Student(10, "Surname1"));
-        list.add(new Student(20, "Surname2"));
-        list.add(new Student(30, "Surname3"));
-
-        Map<String, Student> map = list.stream()
-                .distinct()
-                .collect(Collectors.toMap(
+    public static Map<String, Student> listToMap(List<Student> list){
+        return list.stream()
+                .collect( Collectors.toMap(
                         Student::getSurname,
                         e -> e
                 ));
     }
-
 }
---
+++ src/test/java/ru/job4j/stream/ListToMapTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ListToMapTest {
+
+    @Test
+    public void listToMaptest() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(10, "Surname1"));
+        list.add(new Student(20, "Surname2"));
+        list.add(new Student(30, "Surname3"));
+        Map<String, Student> map = ListToMap.listToMap(list);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname1", new Student(10, "Surname1"));
+        expected.put("Surname2", new Student(20, "Surname2"));
+        expected.put("Surname3", new Student(30, "Surname3"));
+        assertThat(map, is(expected));
+    }
+
+}
